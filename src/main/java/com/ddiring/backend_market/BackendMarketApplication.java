d287d849e35c0ef0aa04bb33fc25564e4aa11778
rlaalsdn0421 <85922540+rlaalsdn0421@users.noreply.github.com>
2025-07-25 13:25:25 +0900

    초기 설정

---
+++ src/main/java/com/ddiring/backend_market/BackendMarketApplication.java
@@ -0,0 +1,13 @@
+package com.ddiring.backend_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMarketApplication.class, args);
+	}
+
+}
