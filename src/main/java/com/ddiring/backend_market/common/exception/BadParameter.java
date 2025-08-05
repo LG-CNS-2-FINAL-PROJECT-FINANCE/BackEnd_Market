813c052040edaee8c309ceb3c43fcb33077a57b8
dohy0103 <kdohy0103@gmail.com>
2025-08-05 14:46:24 +0900

    Add: common error

--- src/main/java/com/ddiring/backend_market/common/exception/BadParameter.java
+++ src/main/java/com/ddiring/backend_market/common/exception/BadParameter.java
@@ -2,6 +2,7 @@ package com.ddiring.backend_market.common.exception;
 
 public class BadParameter extends  ClientError {
     public BadParameter(String message) {
+
         this.errorCode = "BadParameter";
         this.errorMessage = message;
     }
