ad27d2bbfc15e6ab4413d3a710ff2a442b59b4fe
rlaalsdn0421 <85922540+rlaalsdn0421@users.noreply.github.com>
2025-08-12 13:46:45 +0900

    asd

--- src/main/java/com/ddiring/backend_market/BackendMarketApplication.java
+++ src/main/java/com/ddiring/backend_market/BackendMarketApplication.java
@@ -2,10 +2,13 @@ package com.ddiring.backend_market;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
-public class BackendMarketApplication {
-
+@EnableFeignClients
+public class BackendMarketApplication
+{
 	public static void main(String[] args) {
 		SpringApplication.run(BackendMarketApplication.class, args);
 	}
