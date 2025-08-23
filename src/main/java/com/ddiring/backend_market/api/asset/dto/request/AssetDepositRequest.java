ac0ce86ed98ae39126aa5724153058c591bd5d31
rlaalsdn0421 <85922540+rlaalsdn0421@users.noreply.github.com>
2025-08-23 15:36:27 +0900

    에러 수정

--- src/main/java/com/ddiring/backend_market/api/asset/dto/request/AssetDepositRequest.java
+++ src/main/java/com/ddiring/backend_market/api/asset/dto/request/AssetDepositRequest.java
@@ -1,8 +1,5 @@
 package com.ddiring.backend_market.api.asset.dto.request;
 
 public class AssetDepositRequest {
-    public String userSeq;
-    public String projectId;
-    public String role;
-    public Integer price;
+    public Long deposit;
 }
