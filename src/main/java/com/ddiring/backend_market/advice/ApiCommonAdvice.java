fc35cde6eec7b4ec516521696520b6df208043a2
rlaalsdn0421 <85922540+rlaalsdn0421@users.noreply.github.com>
2025-08-26 17:47:59 +0900

    오류수정중

--- src/main/java/com/ddiring/backend_market/advice/ApiCommonAdvice.java
+++ src/main/java/com/ddiring/backend_market/advice/ApiCommonAdvice.java
@@ -20,6 +20,8 @@ public class ApiCommonAdvice {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler({Exception.class})
     public ApiResponseDto<String> handleException(Exception e) {
+        log.warn(e.getMessage());
+
         return ApiResponseDto.createError("ServerError", "서버 에러입니다.");
     }
 
