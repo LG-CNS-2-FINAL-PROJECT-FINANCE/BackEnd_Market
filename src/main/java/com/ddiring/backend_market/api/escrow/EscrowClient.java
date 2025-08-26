030682269c05d56138c5e46801ad614e52e26781
rlaalsdn0421 <85922540+rlaalsdn0421@users.noreply.github.com>
2025-08-26 10:27:54 +0900

    망해도 모름

--- src/main/java/com/ddiring/backend_market/api/escrow/EscrowClient.java
+++ src/main/java/com/ddiring/backend_market/api/escrow/EscrowClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "escrow-service", url = "${escrow-service.url}")
+@FeignClient(name = "escrow-service", url = "${escrow-base.url}")
 public interface EscrowClient {
 
     @PostMapping("/api/escrow/internal/settle-trade")
