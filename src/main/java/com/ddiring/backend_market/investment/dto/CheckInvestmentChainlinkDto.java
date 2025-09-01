ff6c0698e5e46bc3d77846eafb714b69a9fd2a15
NamSangwon <nsw3265@naver.com>
2025-09-01 14:15:06 +0900

    Add : 체인링크 투자 검증 DTO 구성

---
+++ src/main/java/com/ddiring/backend_market/investment/dto/CheckInvestmentChainlinkDto.java
@@ -0,0 +1,50 @@
+package com.ddiring.backend_market.investment.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CheckInvestmentChainlinkDto {
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Request {
+        @NotNull
+        private List<Investment> investments;
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Investment {
+        @NotBlank
+        private String investmentId;
+
+        @NotBlank
+        private String investorAddress;
+
+        @NotEmpty
+        private Long tokenAmount;
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Response {
+        @NotNull
+        private List<Boolean> result;
+    }
+}
