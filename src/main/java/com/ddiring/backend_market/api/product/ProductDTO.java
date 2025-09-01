0e95874ad5a820fd69f5da8df1f7384df5dc52a0
dohy0103 <kdohy0103@gmail.com>
2025-09-01 15:57:09 +0900

    Refactor: 주문 수정 사항
    1. 최대 금액 설정(목표 금액 - 모금액)
    2. 자기 자신이 올린 상품에 투자 불가능

--- src/main/java/com/ddiring/backend_market/api/product/ProductDTO.java
+++ src/main/java/com/ddiring/backend_market/api/product/ProductDTO.java
@@ -12,11 +12,13 @@ import java.time.LocalDate;
 public class ProductDTO {
 
     private String projectId; // 상품 번호
+    private String userSeq; // 사용자 번호 (등록자)
     private String title; // 제목
     private String content; // 본문
     private String summary; // 요약
     private Integer minInvestment;
     private String account; // 모집 계좌
+    private Integer goalAmount; // 목표금액
     private Integer amount; // 모금액
     private LocalDate endDate; // 종료일
     private Integer deadline; // 마감 기한
