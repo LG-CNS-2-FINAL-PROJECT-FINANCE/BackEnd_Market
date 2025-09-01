7448b44b50710f3d68032c0ea0f91d3be834e004
NamSangwon <nsw3265@naver.com>
2025-09-01 15:36:37 +0900

    Add : 거래 체인링크 검증 DTO 구성

---
+++ src/main/java/com/ddiring/backend_market/trade/dto/VerifyTradeDto.java
@@ -0,0 +1,36 @@
+package com.ddiring.backend_market.trade.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class VerifyTradeDto {
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Request {
+        @NotNull
+        private Long tradeId;
+
+        @NotNull
+        private Integer buyId;
+
+        @NotNull
+        private Integer sellId;
+
+        @NotNull
+        private Integer tokenAmount;
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Response {
+        @NotNull
+        private Boolean result;
+    }
+}
