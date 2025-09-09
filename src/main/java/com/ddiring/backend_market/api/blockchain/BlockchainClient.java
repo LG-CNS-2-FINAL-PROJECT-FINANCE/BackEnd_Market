0c6655cf08c9bd59fce9ca249b55c5dbb08ad3d2
rlaalsdn0421 <85922540+rlaalsdn0421@users.noreply.github.com>
2025-09-09 09:19:18 +0900

    maybe 찐찐찐찐찐찐찐찐찐찐찐찐찐찐찐찐찐찐찐찐찐찐찐찐찐찐찐찐막

--- src/main/java/com/ddiring/backend_market/api/blockchain/BlockchainClient.java
+++ src/main/java/com/ddiring/backend_market/api/blockchain/BlockchainClient.java
@@ -11,7 +11,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "blockchainClient", url = "${blockchain-connector.base-url}")
+@FeignClient(name = "blockchainClient", url = "${blockchain.base-url}")
 public interface BlockchainClient {
 
     @PostMapping("/api/contract/investment")
